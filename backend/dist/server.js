e988d59df408dcbe8ce5d31972038d7e32009855
divyansh884 <divyanshsharma35403@gmail.com>
2026-06-15 03:16:41 +0530

    fix: change env var

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -11,7 +11,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/resource-adda";
+  process.env.MONGO_URI || "mongodb://localhost:27017/resource-adda";
 mongoose_1.default
   .connect(MONGODB_URI)
   .then(() => {
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/resource-adda";
+  process.env.MONGO_URI || "mongodb://localhost:27017/resource-adda";
 
 mongoose
   .connect(MONGODB_URI)
